9050a36275efae20da6cc5c02f2eddb3b44b4a78
Spencer Owen <owenspencer@gmail.com>
2015-12-30 11:42:36 -0700

    Updates readme

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,10 @@ name             'ad-join'
 maintainer       'NetDocuments'
 maintainer_email 'sowen@netdocuments.com'
 license          'All rights reserved'
-description      'Joins windows computers to AD Domain'
+description      'Joins windows computers to Active Directory (LDAP) Domain'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.1'
 
 depends 'windows', '>= 1.36.0'
+
+supports 'windows'
