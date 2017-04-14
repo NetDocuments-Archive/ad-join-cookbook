7f06225e2c8f6019628a0e6a6ec684a4d9cd3ed8
Spencer Owen <owenspencer@gmail.com>
2017-04-14 11:13:52 -0600

    Depends on chef 12

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,12 @@ maintainer_email 'sowen@netdocuments.com'
 license          'All rights reserved'
 description      'Joins windows computers to Active Directory (LDAP) Domain'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url       'https://github.com/NetDocuments/ad-join-cookbook/issues'
+source_url       'https://github.com/NetDocuments/ad-join-cookbook'
 version          '4.12.0'
 
 depends 'windows', '>= 1.36.0'
 
 supports 'windows'
+
+chef_version '~> 12'
